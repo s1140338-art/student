beadc8b2127b2e605d46afd13ff5a0bf0e777440
s1140338-art <s1140338@dmhs.kh.edu.tw>
2025-10-27 16:30:59 +0800

    Add files via upload

---
+++ hw2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+    int age = 15;
+    double height = 164;
+    std::string name = "陳冠叡raymond";
+    bool is_student = false;
+    std::cout << "name:" << name << std::endl;
+    std::cout << "身高:" << height << std::endl;
+    std::cout << "年齡:" << age << std::endl;
+    std::cout << "是否為學生:" << is_student << std::endl;
+    
+    // std::cout << "name"　<< name << std::endl；
+    // std::cout << "身高:　"　<< height　<<　std::endl；
+    // std::cout << "年齡:　"　<< age <<　std::endl；
+    // std::cout << "是否為學生:　"　<<　is_shttps:　<<　std::endl；
+    
+    
+    return 0;
+}
